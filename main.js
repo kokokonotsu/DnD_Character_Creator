92356e4f251613db7a1b76e979f508375eca47c7
Ethan Simpson <ethansown@gmail.com>
2018-11-12 21:12:02 -0600

    Created Canvas Element

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var numArray = ["Candy", "Apricots", "Spinach"];
 console.log(numArray.map(newArray => newArray.length));
 
 function clock(){
-    var canvas = document.getElementById("clock-canvas");
+    var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     var radius = canvas.height / 2;
     ctx.translate(radius, radius);
@@ -11,6 +11,9 @@ function clock(){
     radius = radius * 0.9;
     drawClock();
     function drawClock(){
-        ctx.ard(0, 0, radius, 0, 2 * Math.PI);
+        ctx.arc(0, 0, radius, 0 , Math.PI * 2);
+        ctx.fillStyle= "white";
+        ctx.fill();
     }
 }
+window.addEventListener("load", clock);
